e72d2cc58a393736a614ee06e62e6dccb7bd20e4
Shakil53 <shakilahmed.dcc@gmail.com>
2023-09-13 22:03:27 +0600

    menu item click and data pass into mongodb

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,8 +1,57 @@
+import { useContext } from "react";
+import { AuthContex } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const FoodCard = ({ item }) => {
-    const { name, image, price, recipe } = item;
+    const { user } = useContext(AuthContex)
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { name, image, price, recipe, _id } = item;
+
+
+
     const handleAddToCart = (item) => {
         console.log(item)
+        if (user && user.email) {
+            const menuItem = { menuItemId: _id, name, image, price, email: user.email }
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(menuItem)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Food Added on the Cart',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: 'Please login to order the food',
+
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } })
+                }
+            })
+        }
+
+
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
