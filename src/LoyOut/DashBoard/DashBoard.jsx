9d5bf10028f0df6149573d99b8c4baef3fe9bcf5
Shakil53 <shakilahmed.dcc@gmail.com>
2023-09-14 13:43:10 +0600

    dashboard mycart items added successfully

--- src/LoyOut/DashBoard/DashBoard.jsx
+++ src/LoyOut/DashBoard/DashBoard.jsx
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaShoppingCart, FaHome, FaCalendar, FaReceipt, FaWallet, FaBars, FaShopify, FaMailBulk, } from "react-icons/fa";
 
 const DashBoard = () => {
     return (
@@ -6,19 +7,28 @@ const DashBoard = () => {
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 <Outlet></Outlet>
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open Dashboard</label>
 
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side bg-[#D1A054]">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-80 min-h-full">
 
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><NavLink t0='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
+                    <li><NavLink t0='/dashboard/reservations'><FaCalendar></FaCalendar>Reservation</NavLink></li>
+                    <li><NavLink t0='/dashboard/history'><FaWallet></FaWallet>Payment History</NavLink></li>
+                    <li><NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart</NavLink></li>
+                    <li><NavLink to='/'><FaReceipt></FaReceipt>My Booking</NavLink></li>
+                    {/* <li><NavLink to='/'><FaComments></FaComments>My Review</NavLink></li> */}
+                    <div className="divider"></div>
+                    <li><NavLink t0='/'><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink t0='/dashboard/menu'><FaBars></FaBars>Menu</NavLink></li>
+                    <li><NavLink t0='/dashboard/shoppong'><FaShopify></FaShopify>Shopping</NavLink></li>
+                    <li><NavLink t0='/dashboard/contact'><FaMailBulk></FaMailBulk>Contact</NavLink></li>
                 </ul>
 
             </div>
-        </div>
+        </div >
 
     );
 };
--- src/Pages/DashBoard/MyCart.jsx
+++ src/Pages/DashBoard/MyCart.jsx
@@ -1,9 +1,115 @@
-import React from 'react';
+import { Helmet } from 'react-helmet';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import useCart from '../../hooks/useCart';
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyCart = () => {
+    const [cart, refatch] = useCart()
+    // console.log(cart)
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
+
+    // delete item
+    // const deleteHandle = (item) => {
+    //     console.log(item)
+    //     Swal.fire({
+    //         title: 'Are you sure?',
+    //         text: "You want to Delete this item?",
+    //         icon: 'warning',
+    //         showCancelButton: true,
+    //         confirmButtonColor: '#3085d6',
+    //         cancelButtonColor: '#d33',
+    //         confirmButtonText: 'Yes, delete it!'
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             fetch(`http://localhost:5000/carts/${item._id}`, {
+    //                 method: "DELETE",
+
+    //             })
+    //                 .then(res => res.json())
+    //                 .then(data => {
+    //                     if (data.deletedCount > 0) {
+    //                         refatch()
+    //                         Swal.fire(
+    //                             'Deleted!',
+    //                             'Your file has been deleted.',
+    //                             'success'
+    //                         )
+    //                     }
+    //                 })
+    //         }
+    //     })
+    // }
+
     return (
-        <div>
-            <h2>My Cart</h2>
+        <div className='w-full ml-20 mt-8'>
+            <Helmet>
+                <title>
+                    Bistro Boss | My Cart
+                </title>
+            </Helmet>
+
+            <SectionTitle subHeading={"My Cart"}
+                heading={"Add More"}>
+
+            </SectionTitle>
+            <div className='uppercase font-semibold h-[60px] flex justify-evenly items-center'>
+                <h3 className='text-2xl mr-3'>Total Items: {cart.length}</h3>
+                <h3 className='text-2xl mr-3'>Total Price: {total}</h3>
+                <button className="btn btn-sm btn-outline btn-warning">Pay</button>
+
+            </div>
+            {/* table------------------- */}
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} />
+                                        </div>
+                                    </div>
+
+
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td className='text-end'>${item.price}</td>
+                                <th>
+                                    <button className="btn btn-ghost  bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+                                </th>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                    {/* foot */}
+
+
+                </table>
+            </div>
+
+
         </div>
     );
 };
