56e1533810e13dcca669210e755141980f9d6132
Shakil53 <shakilahmed.dcc@gmail.com>
2023-09-15 11:01:02 +0600

    jwt access token create into the local storage and if user logout it will automaticlly remove token

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../Firebase/firebase.config";
 import { GoogleAuthProvider } from "firebase/auth";
+import axios from "axios";
 
 export const AuthContex = createContext(null)
 const auth = getAuth(app);
@@ -54,6 +55,21 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             // console.log('current user', currentUser);
+
+            //    axios using 
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        // console.log(data.data.token)
+                        localStorage.setItem('access-token', data.data.token)
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+            }
+
+
+
             setLoading(false);
         });
         return () => {
